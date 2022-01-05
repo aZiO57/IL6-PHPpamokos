a5f5ffb9a8877f43d1715d6c0bcb7e61a3e7343f
Deividask123 <deividask123@gmail.com>
2022-01-05 20:57:00 +0200

    Penkta pamoka

---
+++ forms/functions.php
@@ -0,0 +1,30 @@
+<?php
+// // $_GET
+// $email=$_POST['user_email'];
+
+// function isEmailValid($email)
+// {
+//     if(strpos($email, "@")) {
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+
+// if(isEmailValid($email)){
+//     echo 'email geras';
+// }else{
+//     echo 'pamirsai @';
+// }
+if ($_POST){
+    if ($_POST ["veiksmas"] == "+")
+        echo $_POST ["number"] + $_POST["number2"];
+    if ($_POST ["veiksmas"] == "-")
+        echo $_POST ["number"] - $_POST["number2"];  
+    if ($_POST ["veiksmas"] == "*")
+        echo $_POST ["number"] * $_POST["number2"];  
+    if ($_POST ["veiksmas"] == "/")
+        echo $_POST ["number"] / $_POST["number2"];      
+}
+
+?>
---
+++ forms/index.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+    <title> Mano title </title>
+</head>
+<body>
+        <div class="header">
+            <ul>
+                <li>
+                    <a href="https://google.lt">Home</a>
+                </li>
+                <li>
+                    <a href="https://delfi.lt">About us</a>
+                </li>
+                <li>
+                    <a href="https://15min.lt">log in</a>
+                </li>
+            </ul>
+        </div>       
+        <div class="content">
+                <h1>Mano Title</h1>
+                <p>Zemiau bandziau suprogramuoti skaiciuotuva...</p>
+            <form action="functions.php" method="post">
+            <form action="functions.php" method="post">
+                     <input type="number" name="number">
+                     <select name="veiksmas">
+                        <option value="*">*</option>
+                        <option value="-">-</option>
+                        <option value="+">+</option>
+                        <option value="/">/</option>
+                    </select>
+                    <input type="number" name="number2">
+                    <input type="submit" value="skaiciuoti" name="submit">
+</form>
+            </form>
+        <div> 
+    </body>
+</html>
