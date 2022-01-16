db097f6e351d04cd82a53d03fe3ed02d1d0fdd99
Deividask123 <deividask123@gmail.com>
2022-01-16 14:50:40 +0200

    TextArea

---
+++ registration/index.php
@@ -0,0 +1,54 @@
+<?php
+include 'formHelper.php';
+$inputs = [
+    [
+        'type' => 'text',
+        'name' => 'name',
+        'placeholder' => 'Vardas'
+    ],
+    [
+        'type' => 'text',
+        'name' => 'last_name',
+        'placeholder' => 'Pavarde'
+    ],
+    [
+        'type' => 'password',
+        'name' => 'password',
+        'placeholder' => '*******'
+    ],
+    [
+        'type' => 'password',
+        'name' => 'password2',
+        'placeholder' => '*******'
+    ],
+    [
+        'type' => 'submit',
+        'name' => 'register',
+        'value' => 'Registruotis'
+    ],
+    [
+        'type' => 'select',
+        'name' => 'childrens_number',
+        'options' => [0, 1, 2, 3, '4+']
+    ],
+    [
+        'type' => 'textarea',
+        'name' => 'comment'
+    ]
+];
+
+echo '<form action="registration.php" method="post">';
+
+foreach ($inputs as $input) {
+    if ($input['type'] !== 'select') {
+        echo generateInput($input) . '<br>';
+    } else {
+        echo generateSelect($input);
+    }elseif($input['type'] === 'textarea'){
+        echo generateTextArea($input);
+    }else
+}
+// Sukurti textarea !!!!!!!!!!!
+?>   
+
+
