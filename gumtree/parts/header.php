d91729fb8e4b2dd48aa592e510968baf8b512ae5
Deividask123 <deividask123@gmail.com>
2022-01-19 19:53:18 +0200

    advertisement posting

---
+++ gumtree/parts/header.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>Create ad</title>
+</head>
+<body>
+<div class="header">
+    <ul class="menu">
+        <li><a href="create.php">Add Ad!</a></li>
+        <li><a href="catalog.php">All ads!</a></li>
+    </ul>
+</div>
+<div class="content">
---
+++ gumtree/submitad.php
@@ -0,0 +1,29 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbName = 'auto_minusas';
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=".$dbName, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+
+if(isset($_POST['create'])){
+    $title = $_POST['title'];
+    $content = $_POST['content'];
+    $price = $_POST['price'];
+    $year = $_POST['years'];
+
+    $sql = 'INSERT INTO ads (title, description, manufacturer_id, model_id, price, years, type_id, user_id) 
+            VALUES ("'.$title.'", "'.$content.'", 1, 1, '.$price.', '.$year.', 1, 1)';
+
+    echo $sql;
+    $conn->query($sql);
+
+}
