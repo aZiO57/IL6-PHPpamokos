c91debdde020eb640109c7ded98e416dd1b73334
Deividask123 <deividask123@gmail.com>
2022-01-26 19:32:28 +0200

    composer init

--- oop/index.php
+++ oop/index.php
@@ -1,8 +1,25 @@
 <?php
 
+include 'vendor/autoload.php';
 
 if (isset($_SERVER['PATH_INFO']) && $_SERVER['PATH_INFO'] !== '/') {
-    echo $_SERVER('PATH_INFO');
+    $path = trim($_SERVER['PATH_INFO'], '/');
+    echo '<pre>';
+    $path = explode('/', $path);
+    print_r($path);
+    $class = ucfirst($path[0]);
+    $method = $path[1];
+    $param = $path[2];
+    //echo 'app/code/Controller/'.$class.'.php';
+
+    //    $obj = new $class();
+    $class = '\Controller\\' . $class;
+    $obj = new $class();
+
+
+    $obj->$method($param);
 } else {
     echo 'home page';
 }
+
+// domain.lt/controlleris/methodas/params
---
+++ oop/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '8bf379bf35dbc6268aff7adaa73bb385a64c7e1b',
+        'name' => 'deividas/skelbimai',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'deividas/skelbimai' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '8bf379bf35dbc6268aff7adaa73bb385a64c7e1b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
