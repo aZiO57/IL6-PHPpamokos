5946e89131003d2081a1899c53b6cc2e92243b6f
Deividask123 <deividask123@gmail.com>
2022-04-19 19:19:38 +0300

    Second init

---
+++ gandas.lt/app/code/Controller/News.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Controller;
+
+use Core\ControllerAbstract;
+
+class News extends ControllerAbstract
+{
+
+    public function show(string $slug)
+    {
+        $new = new \Model\News();
+        $new->loadBySlug($slug);
+        echo $this->twig->render('news/single.html', ['new' => $new]);
+    }
+
+    public function all()
+    {
+        $news = new \Model\Collections\News();
+        $news->filter('active', 1);
+        echo $this->twig->render('news/all.html', ['news' => $news->get()]);
+    }
+}
--- gandas.lt/app/code/Core/ModelAbstract.php
+++ gandas.lt/app/code/Core/ModelAbstract.php
@@ -6,27 +6,61 @@ use Aura\SqlQuery\QueryFactory;
 
 class ModelAbstract
 {
+    protected $id;
+
     protected QueryFactory $queryFactory;
+
+    protected DB $db;
+
+    protected const TABLE = '';
 
     public function __construct()
     {
         $this->queryFactory = new QueryFactory('mysql');
+        $this->db = new DB();
     }
 
     protected function select()
     {
         return  $this->queryFactory->newSelect();
     }
+
     protected function insert()
     {
-        return $this->queryFactory->newInser();
+        return  $this->queryFactory->newInsert();
     }
+
     protected function update()
     {
         return $this->queryFactory->newUpdate();
     }
+
+
     protected function delete()
     {
         return $this->queryFactory->newDelete();
+    }
+
+    public function save(): void
+    {
+        $this->assignData();
+        $this->create();
+    }
+
+    protected function assignData(): void
+    {
+        $this->data = [];
+    }
+    protected function create()
+    {
+        $insert = $this->insert();
+        $insert->into(static::TABLE)->cols($this->data);
+        $this->db->execute($insert);
+    }
+    protected function edit(): void
+    {
+        $update = $this->update();
+        $update->table(static::TABLE)->cols($this->data);
+        $this->db->execute($update);
     }
 }
